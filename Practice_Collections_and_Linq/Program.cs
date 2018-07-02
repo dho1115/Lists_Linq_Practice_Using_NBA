0257e86a1d731781ca359f415fc17a3a598aef52
David H <davidhorph@hotmail.com>
2018-07-01 19:28:34 -0500

    Add project files.

---
+++ Practice_Collections_and_Linq/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practice_Collections_and_Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
